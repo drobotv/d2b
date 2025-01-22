67b37d05aa032f4b6467120246266d6775be6bc8
drobotv <drobotvladik@gmail.com>
2025-01-22 16:36:38 +0100

    chore: add valibot - replace zod - valibot has better i18n support

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,6 @@
 import { i18n } from "$lib/i18n";
 import * as auth from "$lib/server/auth";
-import type { Handle } from "@sveltejs/kit";
+import { redirect, type Handle } from "@sveltejs/kit";
 import { sequence } from "@sveltejs/kit/hooks";
 
 const handleAuth: Handle = async ({ event, resolve }) => {
@@ -8,7 +8,8 @@ const handleAuth: Handle = async ({ event, resolve }) => {
   if (!token) {
     event.locals.user = null;
     event.locals.session = null;
-    return resolve(event);
+    redirect(302, "/login");
+    // return resolve(event);
   }
 
   const { session, user } = await auth.validateSessionToken(token);
